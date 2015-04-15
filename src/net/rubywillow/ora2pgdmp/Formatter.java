31741dcdc46f11521a60c2927b6348ee3677745a
djbckr <djbckr@Deans-iMac-2.local>
2015-04-15 15:38:04 -0700

    Optimizations; made character escaping much faster.

--- src/net/rubywillow/ora2pgdmp/Formatter.java
+++ src/net/rubywillow/ora2pgdmp/Formatter.java
@@ -22,282 +22,328 @@ import java.util.concurrent.locks.ReentrantLock;
 */
 public class Formatter implements Callable<Integer> {
 
-  // if a date comes back as a java.sql.Timestamp, use this date formatter
-  private SimpleDateFormat sdf;
+    private static final int backslash = 92;
+    private static final int linefeed = 10;
+    private static final int carriagereturn = 13;
+    private static final int tabchar = 9;
 
-  // The file we write to. Writes must be synchronized using lock
-  private Writer out;
-  private ReentrantLock lock;
+    // if a date comes back as a java.sql.Timestamp, use this date formatter
+    private SimpleDateFormat sdf;
 
-  // our main buffer
-  private StringBuilder buf = new StringBuilder(1024);
+    // The file we write to. Writes must be synchronized using lock
+    private Writer out;
+    private ReentrantLock lock;
 
-  // the columns we are formatting
-  private Object[] cols;
+    // our main buffer
+    private StringBuilder buf = new StringBuilder(1024);
 
-  private final static int TWOMEG = 2097152;
-  private final static int ONEMEG = 1048576;
+    // the columns we are formatting
+    private Object[] cols;
 
-  public Formatter(Writer out_, Object[] cols_, ReentrantLock lock_) {
-    out = out_;
-    cols = cols_;
-    lock = lock_;
-  }
+    private final static int TWOMEG = 2097152;
+    private final static int ONEMEG = 1048576;
 
-  @Override
-  public Integer call() throws Exception {
-
-    if (cols == null)
-      // this is our indication that we aren't processing any more
-      return 1;
-
-    try {
-      String tab = null;
-      // iterate through the columns, putting a tab
-      // character between them. (no tab at beginning or end)
-      for ( Object obj : cols ) {
-
-        if (tab != null)
-          buf.append( tab );
-        else
-          tab = "\t";
-
-        processObject( obj );
-      }
-      // new-line indicates end of record
-      buf.append( "\n" );
-      // push to file
-      flushBuffer(true);
-    } catch ( Exception e ) {
-      e.printStackTrace();
-    }
-    finally {
-      lock.unlock();
-    }
-    Main.recCount.incrementAndGet();
-    return 0;
-  }
-
-  private void doFlush() throws Exception {
-    out.write( buf.toString() );
-    buf.setLength( 0 );
-  }
-
-  private void flushBuffer(boolean force) throws Exception {
-
-    // We *must* write now. Wait indefinitely for a lock.
-    if (force) {
-      if (!lock.isHeldByCurrentThread()) {
-        lock.lock();
-      }
-      doFlush();
-      return;
+    public Formatter(Writer out_, Object[] cols_, ReentrantLock lock_) {
+        out = out_;
+        cols = cols_;
+        lock = lock_;
     }
 
-    // If we aren't forcing, we'll attempt to get a lock but not try too hard.
-    // 1: if we already have the lock OR
-    // 2: get a lock if nothing else is holding the lock OR
-    // 3: try to get the lock but only for 1 second if something else is holding.
-    if (lock.isHeldByCurrentThread() || lock.tryLock() || lock.tryLock( 1, TimeUnit.SECONDS )) {
-      doFlush();
+    @Override
+    public Integer call() throws Exception {
+
+        if (cols == null)
+            // this is our indication that we aren't processing any more
+            return 1;
+
+        try {
+            String tab = null;
+            // iterate through the columns, putting a tab
+            // character between them. (no tab at beginning or end)
+            for (Object obj : cols) {
+
+                if (tab != null)
+                    buf.append(tab);
+                else
+                    tab = "\t";
+
+                processObject(obj);
+            }
+            // new-line indicates end of record
+            buf.append("\n");
+            // push to file
+            flushBuffer(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+        Main.recCount.incrementAndGet();
+        return 0;
     }
 
-  }
-
-  private void checkFlush() throws Exception {
-
-    int len = buf.length();
-
-    if (len < ONEMEG) // don't need to flush yet
-      return;
-
-    if (len < TWOMEG) {
-      // try to flush, but it's not critical yet
-      flushBuffer( false );
-      return;
+    private void doFlush() throws Exception {
+        out.write(buf.toString());
+        buf.setLength(0);
     }
 
-    // must flush; buffer getting too big now
-    flushBuffer( true );
-  }
+    private void flushBuffer(boolean force) throws Exception {
 
-  private void doWrite( String str ) throws Exception {
-    buf.append( str );
-    checkFlush();
-  }
+        // We *must* write now. Wait indefinitely for a lock.
+        if (force) {
+            if (!lock.isHeldByCurrentThread()) {
+                lock.lock();
+            }
+            doFlush();
+            return;
+        }
 
-  private void doWrite( final char[] charArray ) throws Exception {
-    buf.append( charArray );
-    checkFlush();
-  }
+        // If we aren't forcing, we'll attempt to get a lock but not try too hard.
+        // 1: if we already have the lock OR
+        // 2: get a lock if nothing else is holding the lock OR
+        // 3: try to get the lock but only for 1 second if something else is holding.
+        if (lock.isHeldByCurrentThread() || lock.tryLock() || lock.tryLock(1, TimeUnit.SECONDS)) {
+            doFlush();
+        }
 
-  // this goes through all of the data types that we'll support. There
-  // are more (like objects and collections) and maybe we'll support
-  // collections in the future.
-  private void processObject( Object obj ) throws Exception {
-
-    if ( obj == null ) {
-      doWrite( "\\N" );
-      return;
     }
 
-    if ( obj instanceof java.lang.String ) {
-      escape( (String) obj );
-      return;
+    private void checkFlush() throws Exception {
+
+        int len = buf.length();
+
+        if (len < ONEMEG) // don't need to flush yet
+            return;
+
+        if (len < TWOMEG) {
+            // try to flush, but it's not critical yet
+            flushBuffer(false);
+            return;
+        }
+
+        // must flush; buffer getting too big now
+        flushBuffer(true);
     }
 
-    if ( obj instanceof oracle.sql.NUMBER ) {
-      doWrite( ( (oracle.sql.NUMBER) obj ).stringValue() );
-      return;
+    private void doWrite(final String str) throws Exception {
+        buf.append(str);
+        checkFlush();
     }
 
-    if ( obj instanceof java.math.BigDecimal ) {
-      doWrite( ( (BigDecimal) obj ).toPlainString() );
-      return;
+    private void doWrite(final char[] charArray) throws Exception {
+        buf.append(charArray);
+        checkFlush();
     }
 
-    if ( obj instanceof oracle.sql.CHAR ) {
-      escape( ( (oracle.sql.CHAR) obj ).stringValue() );
-      return;
+    // this goes through all of the data types that we'll support. There
+    // are more (like objects and collections) and maybe we'll support
+    // collections in the future.
+    private void processObject(Object obj) throws Exception {
+
+        if (obj == null) {
+            doWrite("\\N");
+            return;
+        }
+
+        if (obj instanceof java.lang.String) {
+            escape((String) obj);
+            return;
+        }
+
+        if (obj instanceof oracle.sql.NUMBER) {
+            doWrite(((oracle.sql.NUMBER) obj).stringValue());
+            return;
+        }
+
+        if (obj instanceof java.math.BigDecimal) {
+            doWrite(((BigDecimal) obj).toPlainString());
+            return;
+        }
+
+        if (obj instanceof oracle.sql.CHAR) {
+            escape(((oracle.sql.CHAR) obj).stringValue());
+            return;
+        }
+
+        if (obj instanceof oracle.sql.DATE) {
+            doWrite(((oracle.sql.DATE) obj).stringValue());
+            return;
+        }
+
+        if (obj instanceof java.sql.Timestamp) {
+
+            if (sdf == null)
+                sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+            doWrite(sdf.format((Timestamp) obj));
+            return;
+        }
+
+        if (obj instanceof oracle.sql.TIMESTAMP) {
+            doWrite(((oracle.sql.TIMESTAMP) obj).stringValue());
+            return;
+        }
+
+        if (obj instanceof oracle.sql.TIMESTAMPTZ) {
+            doWrite(((oracle.sql.TIMESTAMPTZ) obj).stringValue());
+            return;
+        }
+
+        if (obj instanceof oracle.sql.TIMESTAMPLTZ) {
+            doWrite(((oracle.sql.TIMESTAMPLTZ) obj).stringValue());
+            return;
+        }
+
+        if (obj instanceof oracle.sql.INTERVALYM) {
+            doWrite(((oracle.sql.INTERVALYM) obj).stringValue());
+            return;
+        }
+
+        if (obj instanceof oracle.sql.INTERVALDS) {
+            doWrite(((oracle.sql.INTERVALDS) obj).stringValue());
+            return;
+        }
+
+        if (obj instanceof byte[]) {
+            bytesToHex((byte[]) obj, true);
+            return;
+        }
+
+        if (obj instanceof oracle.sql.RAW) {
+            bytesToHex(((oracle.sql.RAW) obj).getBytes(), true);
+            return;
+        }
+
+        if (obj instanceof oracle.sql.CLOB) {
+            final Reader r = ((oracle.sql.CLOB) obj).getCharacterStream();
+
+            char[] buf = new char[16384];
+            int readAmount = r.read(buf);
+
+            while (readAmount > 0) {
+                escape(new String(buf, 0, readAmount));
+                readAmount = r.read(buf);
+            }
+            return;
+        }
+
+        if (obj instanceof oracle.sql.BLOB) {
+            final InputStream r = ((BLOB) obj).getBinaryStream();
+
+            byte[] buf = new byte[16384];
+            int readAmount = r.read(buf);
+
+            boolean head = true;
+            while (readAmount > 0) {
+                bytesToHex(buf, 0, readAmount, head);
+                if (head)
+                    head = false;
+                readAmount = r.read(buf);
+            }
+
+            return;
+        }
+
+        if (obj instanceof java.lang.Float) {
+            doWrite(new BigDecimal((Float) obj).toPlainString());
+            return;
+        }
+
+        if (obj instanceof java.lang.Double) {
+            doWrite(new BigDecimal((Double) obj).toPlainString());
+            return;
+        }
+
+        if (obj instanceof oracle.sql.ANYDATA) {
+            // recursive call
+            processObject(((ANYDATA) obj).accessDatum());
+            return;
+        }
+
+        if (obj instanceof oracle.sql.BINARY_DOUBLE) {
+            doWrite(((oracle.sql.BINARY_DOUBLE) obj).bigDecimalValue().toPlainString());
+            return;
+        }
+
+        if (obj instanceof oracle.sql.BINARY_FLOAT) {
+            doWrite(((oracle.sql.BINARY_FLOAT) obj).bigDecimalValue().toPlainString());
+            return;
+        }
+
+        throw new SQLException("An unsupported datatype was encountered: " + obj.getClass().getCanonicalName());
     }
 
-    if ( obj instanceof oracle.sql.DATE ) {
-      doWrite( ( (oracle.sql.DATE) obj ).stringValue() );
-      return;
+    private void escape(final String str) throws Exception {
+
+        if (str.isEmpty())
+            return;
+
+        // PostgreSQL COPY data cannot have carriage returns, line-feeds, tab characters, or single backslashes
+        // replace these with "\r", "\n", "\t", "\\" in the final output
+
+        if ((str.indexOf(backslash, 0) == -1) &&
+                (str.indexOf(linefeed) == -1) &&
+                (str.indexOf(tabchar) == -1) &&
+                (str.indexOf(carriagereturn) == -1)) {
+            // if there are no special characters, just write the string as-is
+            doWrite(str);
+        } else {
+            // It's fastest to manipulate this as an array of characters (using StringBuilder of course)
+            StringBuilder sb = new StringBuilder(str);
+
+            // easiest to start at the end and work backwards (and probably faster)
+            int j = sb.length();
+            int i = j;
+            i--;
+
+            while (i >= 0) {
+                int x = sb.charAt(i);
+                switch (x) {
+                    case backslash:
+                        sb.replace(i, j, "\\\\");
+                        break;
+                    case linefeed:
+                        sb.replace(i, j, "\\n");
+                        break;
+                    case carriagereturn:
+                        sb.replace(i, j, "\\r");
+                        break;
+                    case tabchar:
+                        sb.replace(i, j, "\\t");
+                        break;
+                    default:
+                        break;
+                }
+                i--;
+                j--;
+            }
+            doWrite(sb.toString());
+        }
     }
 
-    if ( obj instanceof java.sql.Timestamp ) {
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
-      if (sdf == null)
-        sdf = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss.SSS" );
+    private void bytesToHex(final byte[] bytes, final int offset, final int length, final boolean header) throws Exception {
+        int offs = offset;
+        int i = 0;
+        int j = 0;
+        int v;
 
-      doWrite( sdf.format( (Timestamp) obj ) );
-      return;
+        char[] hexChars = new char[length * 2];
+        while (i++ < length) {
+            v = bytes[offs++] & 0xFF;
+            hexChars[j++] = hexArray[v >>> 4];
+            hexChars[j++] = hexArray[v & 0x0F];
+        }
+
+        if (header)
+            doWrite("\\\\x");
+
+        doWrite(hexChars);
     }
 
-    if ( obj instanceof oracle.sql.TIMESTAMP ) {
-      doWrite( ( (oracle.sql.TIMESTAMP) obj ).stringValue() );
-      return;
+    private void bytesToHex(final byte[] bytes, final boolean header) throws Exception {
+        bytesToHex(bytes, 0, bytes.length, header);
     }
-
-    if ( obj instanceof oracle.sql.TIMESTAMPTZ ) {
-      doWrite( ( (oracle.sql.TIMESTAMPTZ) obj ).stringValue() );
-      return;
-    }
-
-    if ( obj instanceof oracle.sql.TIMESTAMPLTZ ) {
-      doWrite( ( (oracle.sql.TIMESTAMPLTZ) obj ).stringValue() );
-      return;
-    }
-
-    if ( obj instanceof oracle.sql.INTERVALYM ) {
-      doWrite( ( (oracle.sql.INTERVALYM) obj ).stringValue() );
-      return;
-    }
-
-    if ( obj instanceof oracle.sql.INTERVALDS ) {
-      doWrite( ( (oracle.sql.INTERVALDS) obj ).stringValue() );
-      return;
-    }
-
-    if ( obj instanceof byte[] ) {
-      bytesToHex( (byte[]) obj, true );
-      return;
-    }
-
-    if ( obj instanceof oracle.sql.RAW ) {
-      bytesToHex( ( (oracle.sql.RAW) obj ).getBytes(), true );
-      return;
-    }
-
-    if ( obj instanceof oracle.sql.CLOB ) {
-      final Reader r = ( (oracle.sql.CLOB) obj ).getCharacterStream();
-
-      char[] buf = new char[16384];
-      int readAmount = r.read( buf );
-
-      while ( readAmount > 0 ) {
-        escape( new String( buf, 0, readAmount ) );
-        readAmount = r.read( buf );
-      }
-      return;
-    }
-
-    if ( obj instanceof oracle.sql.BLOB ) {
-      final InputStream r = ( (BLOB) obj ).getBinaryStream();
-
-      byte[] buf = new byte[16384];
-      int readAmount = r.read( buf );
-
-      boolean head = true;
-      while ( readAmount > 0 ) {
-        bytesToHex( buf, 0, readAmount, head );
-        if (head)
-          head = false;
-        readAmount = r.read( buf );
-      }
-
-      return;
-    }
-
-    if ( obj instanceof java.lang.Float ) {
-      doWrite( new BigDecimal( (Float) obj ).toPlainString() );
-      return;
-    }
-
-    if ( obj instanceof java.lang.Double ) {
-      doWrite( new BigDecimal( (Double) obj ).toPlainString() );
-      return;
-    }
-
-    if ( obj instanceof oracle.sql.ANYDATA ) {
-      // recursive call
-      processObject( ( (ANYDATA) obj ).accessDatum() );
-      return;
-    }
-
-    if ( obj instanceof oracle.sql.BINARY_DOUBLE ) {
-      doWrite( ( (oracle.sql.BINARY_DOUBLE) obj ).bigDecimalValue().toPlainString() );
-      return;
-    }
-
-    if ( obj instanceof oracle.sql.BINARY_FLOAT ) {
-      doWrite( ( (oracle.sql.BINARY_FLOAT) obj ).bigDecimalValue().toPlainString() );
-      return;
-    }
-
-    throw new SQLException( "An unsupported datatype was encountered: " + obj.getClass().getCanonicalName() );
-  }
-
-  private void escape( final String str ) throws Exception {
-    // PostgreSQL COPY data cannot have carriage returns, line-feeds, tab characters, or single backslashes
-    // replace these with "\r", "\n", "\t", "\\" in the final output
-    doWrite( str.replace( "\\", "\\\\" ).replace( "\t", "\\t" ).replace( "\n", "\\n" ).replace( "\r", "\\r" ) );
-  }
-
-  final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
-
-  private void bytesToHex( final byte[] bytes, final int offset, final int length, final boolean header ) throws Exception {
-    int offs = offset;
-    int i = 0;
-    int j = 0;
-    int v;
-
-    char[] hexChars = new char[length * 2];
-    while ( i++ < length ) {
-      v = bytes[offs++] & 0xFF;
-      hexChars[j++] = hexArray[v >>> 4];
-      hexChars[j++] = hexArray[v & 0x0F];
-    }
-
-    if (header)
-      doWrite( "\\\\x" );
-
-    doWrite( hexChars );
-  }
-
-  private void bytesToHex( final byte[] bytes, final boolean header ) throws Exception {
-    bytesToHex( bytes, 0, bytes.length, header );
-  }
 
 }
